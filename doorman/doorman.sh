9f3e174c63bb84ad32d8b536f7d5cad08f3c4842
Nikolas Coukouma <atrus@atrus.org>
2010-04-20 18:25:51 -0400

    goofy syntax

--- doorman/doorman.sh
+++ doorman/doorman.sh
@@ -6,6 +6,6 @@ TMP_LOG_FILE="$LOG_FILE.new"
 if [ -z "$DOORMAN_DIR" ]; then DOORMAN_DIR=`dirname $0`; fi
 
 cd $DOORMAN_DIR
-ruby doorman.rb 2>&1 >> $LOG_FILE
+ruby doorman.rb 2>&1 | cat >> $LOG_FILE
 tail -n 1000 $LOG_FILE > $TMP_LOG_FILE
 mv $TMP_LOG_FILE $LOG_FILE
